cc15d73068f8c60db0ec586f7e2fda011f6410e2
abhilash <abhilashmohapatra948@gmail.com>
2025-05-29 18:01:50 +0530

    inital commit

---
+++ Backend/Frontend/src/home/Leftpart/User.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
+  return (
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
+      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+             <img src="https://imgs.search.brave.com/7_-25qcHnU9PLXYYiiK-IwkQx93yFpp__txSD1are3s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzY0LzY3LzYz/LzM2MF9GXzY0Njc2/MzgzX0xkYm1oaU5N/NllwemIzRk00UFB1/RlA5ckhlN3JpOEp1/LmpwZw" />
+        
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-bold">{user.fullname}</h1>
+          <span>{user.email}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
---
+++ Backend/Frontend/src/home/Rightpart/Chatuser.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import { CiMenuFries } from "react-icons/ci";
+
+function Chatuser() {
+  const { selectedConversation } = useConversation();
+  const { onlineUsers } = useSocketContext();
+  const getOnlineUsersStatus = (userId) => {
+    return onlineUsers.includes(userId) ? "Online" : "Offline";
+  };
+
+  return (
+    <div className=" pl-5 pt-5 h-[12vh] flex space-x-4 bg-gray-700 hover:bg-gray-600 duration-300">
+      <div>
+        <div className="avatar online">
+          <div className="w-14 rounded-full">
+            <img src="https://imgs.search.brave.com/7_-25qcHnU9PLXYYiiK-IwkQx93yFpp__txSD1are3s/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAwLzY0LzY3LzYz/LzM2MF9GXzY0Njc2/MzgzX0xkYm1oaU5N/NllwemIzRk00UFB1/RlA5ckhlN3JpOEp1/LmpwZw"/>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-xl">{selectedConversation.fullname}</h1>
+        <span className="text-sm">
+          {getOnlineUsersStatus(selectedConversation._id)}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Chatuser;
